6e9dec308985a05a3ed2df54651954c61b69a07b
Zineddine <zinedddine97@gmail.com>
2020-12-10 23:38:21 +0100

    Add some classes

--- progfun-al-2020/src/main/scala/projetal2020/Main.scala
+++ progfun-al-2020/src/main/scala/projetal2020/Main.scala
@@ -1,10 +1,42 @@
 package projetal2020
 
 object Main extends App {
-  println("test prog")
-  // Le code suivant ne compilera pas.
-  // var tmp = null;
-  // var tmp2 = if (tmp == 1) "yes" else 1
 
-  // println(s"tmp: $tmp, tmp2: $tmp2")
+  def grille = new Grid();
+  def tondeuse = new LawnMower(0, 0, Direction.North, grille);
+  tondeuse.move(Instruction.Avancer);
 }
+
+object Direction extends Enumeration {
+  val North, East, West, South = Value
+}
+
+object Instruction extends Enumeration {
+  val Gauche, Droite, Avancer = Value
+}
+
+class Grid {}
+
+class LawnMower(
+    val x: Int,
+    val y: Int,
+    val orientation: Direction.Value,
+    val grid: Grid
+) {
+  // def positions: Array[Int] = Array();
+  // def orientation: Direction.Value;
+  // def grid: Grid;
+
+  // def LawnMower(val x: Int, val y: Int, val orientation: Direction.Value, val3 grid: Grid) = {
+  //   this.positions(0) = x;
+  //   this.positions(1) = y;
+  //   this.orientation = orientation;
+  //   this.grid = grid;
+  // }
+
+  def move(instruction: Instruction.Value) = {
+    println("instruction : " + instruction.toString);
+  }
+}
+
+object FileHandler {}
