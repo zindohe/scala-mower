6f3ec3b0b7ad49869db86ceb0fba13a5c31f6997
Zineddine <zinedddine97@gmail.com>
2020-12-17 21:56:49 +0100

    Add checkPostions method to Executor

--- progfun-al-2020/src/main/scala/projetal2020/Main.scala
+++ progfun-al-2020/src/main/scala/projetal2020/Main.scala
@@ -5,19 +5,20 @@ import java.io._
 
 object Main extends App {
 
-  val configList = FileHandler.read_instructions("instructions.txt").toList
+  val configList = FileHandler.readInstructions("instructions.txt").toList
 
-  val grid = Executor.parse_grid_size(configList.headOption.getOrElse("4 4"))
-  val lawnmowers = Executor.parse_lawnmowers(configList.drop(1))
+  val grid = Executor.parseGridSize(configList.headOption.getOrElse("4 4"))
+  val lawnmowers = Executor.parseLawnmowers(configList.drop(1))
 
   val results = lawnmowers.map(
     (l: LawnMower) =>
       (
         l,
         Executor
-          .calcul_final_pos(
-            new Coordinates(l.x, l.y, l.direction),
-            l.instructions
+          .calculFinalPos(
+            l.coordinates,
+            l.instructions,
+            grid
           )
       )
   )
@@ -26,9 +27,9 @@ object Main extends App {
   //   println(result._1.toString)
   // }
 
-  FileHandler.write_results(
+  FileHandler.writeResults(
     "resultat.json",
-    FileHandler.get_json(results, grid)
+    FileHandler.getJson(results, grid)
   )
 }
 
@@ -84,65 +85,76 @@ class Grid(val height: Int, val width: Int) {
 
 object Executor {
 
-  def start_move(
-      x: Int,
-      y: Int,
-      orientation: Direction.Value,
-      instructions: String
-  ) = {
-    println(x.toString + y.toString + orientation.toString);
-    instructions.split("");
-  }
-
-  def calcul_final_pos(
+  def calculFinalPos(
       result: Coordinates,
-      instructions: List[Instruction.Value]
+      instructions: List[Instruction.Value],
+      grid: Grid
   ): Coordinates = instructions match {
     case value :: rest if value == Instruction.Avancer =>
-      calcul_final_pos(Executor.moveForward(result), rest)
+      calculFinalPos(
+        checkCoordinate(result, Executor.moveForward(result), grid),
+        rest,
+        grid
+      )
     case value :: rest =>
-      calcul_final_pos(
-        new Coordinates(
-          result.x,
-          result.y,
-          Direction.getNewDirection(result.direction, value)
+      calculFinalPos(
+        checkCoordinate(
+          result,
+          new Coordinates(
+            result.x,
+            result.y,
+            Direction.getNewDirection(result.direction, value)
+          ),
+          grid
         ),
-        rest
+        rest,
+        grid
       )
     case Nil => result
   }
 
-  def parse_grid_size(config: String) = {
+  def checkCoordinate(
+      currentCoordinates: Coordinates,
+      newCoordinates: Coordinates,
+      grid: Grid
+  ): Coordinates = newCoordinates match {
+    case n if grid.checkCoordinate(n) => newCoordinates
+    case _                            => currentCoordinates
+  }
+
+  def parseGridSize(config: String) = {
     def sizes = config.split(" ")
     new Grid(sizes(0).toInt, sizes(1).toInt)
   }
 
-  def parse_lawnmowers(lawnmowers: List[String]) = {
-    parse_one_lawnmower(lawnmowers, List[LawnMower]())
+  def parseLawnmowers(lawnmowers: List[String]) = {
+    parseOneLawnmower(lawnmowers, List[LawnMower]())
   }
 
-  def parse_one_lawnmower(
+  def parseOneLawnmower(
       lawnmowers: List[String],
-      lawnmowers_list: List[LawnMower]
+      lawnmowersList: List[LawnMower]
   ): List[LawnMower] = lawnmowers match {
     case n :: m :: rest => {
-      def starting_stats = n.split(" ")
-      def new_lawnmower =
+      def startingStats = n.split(" ")
+      def newLawnmower =
         new LawnMower(
-          starting_stats(0).toInt,
-          starting_stats(1).toInt,
-          Direction
-            .getFromString(starting_stats(2))
-            .getOrElse(Direction.North),
+          new Coordinates(
+            startingStats(0).toInt,
+            startingStats(1).toInt,
+            Direction
+              .getFromString(startingStats(2))
+              .getOrElse(Direction.North)
+          ),
           m.split("")
             .map(
               Instruction.getFromString(_).getOrElse(Instruction.Avancer)
             )
             .toList
         )
-      parse_one_lawnmower(rest, lawnmowers_list ::: List(new_lawnmower))
+      parseOneLawnmower(rest, lawnmowersList ::: List(newLawnmower))
     }
-    case _ => lawnmowers_list
+    case _ => lawnmowersList
   }
 
   def moveForward(coordinates: Coordinates) = coordinates match {
@@ -158,20 +170,12 @@ object Executor {
 }
 
 class LawnMower(
-    val x: Int,
-    val y: Int,
-    val direction: Direction.Value,
+    val coordinates: Coordinates,
     val instructions: List[Instruction.Value]
 ) {
 
-  def toCoordinates(): Coordinates = {
-    new Coordinates(x, y, direction)
-  }
-
   override def toString(): String = {
-    "x : " + x.toString +
-      "\ny : " + y.toString +
-      "\ndirection : " + direction.toString +
+    coordinates.toString +
       "\ninstructions : " + instructions
       .map(_.toString)
       .mkString("")
@@ -192,7 +196,7 @@ class Coordinates(
 }
 
 object FileHandler {
-  def read_instructions(filename: String) = {
+  def readInstructions(filename: String) = {
     Source.fromFile(filename).getLines().map((elem: String) => elem)
   }
 
@@ -206,7 +210,7 @@ object FileHandler {
     )
   }
 
-  def get_json(results: List[(LawnMower, Coordinates)], grid: Grid) = {
+  def getJson(results: List[(LawnMower, Coordinates)], grid: Grid) = {
     Json.prettyPrint(
       Json.toJson(
         Json.obj(
@@ -217,13 +221,7 @@ object FileHandler {
           "tondeuses" -> results.map(
             lawnmower =>
               Json.obj(
-                "debut" -> Json.obj(
-                  "point" -> Json.obj(
-                    "x" -> lawnmower._1.x,
-                    "y" -> lawnmower._1.y
-                  ),
-                  "direction" -> lawnmower._1.direction.toString
-                ),
+                "debut"        -> lawnmower._1.coordinates,
                 "instructions" -> lawnmower._1.instructions,
                 "fin"          -> lawnmower._2
               )
@@ -233,7 +231,7 @@ object FileHandler {
     )
   }
 
-  def write_results(filename: String, results: String) = {
+  def writeResults(filename: String, results: String) = {
     val file = new File(filename)
     val buffer = new BufferedWriter(new FileWriter(file))
     buffer.write(results.toString)
